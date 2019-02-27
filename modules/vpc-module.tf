522b0d34f24482c7bdaf4816235c87f41e3ab9da
topgun93 <38679840+topgun93@users.noreply.github.com>
2019-02-27 12:38:07 +0530

    ec2 sg ingress rule change

--- modules/vpc-module.tf
+++ modules/vpc-module.tf
@@ -122,7 +122,7 @@ resource "aws_security_group" "nginx-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["${cidrsubnet(var.cidr, 8, count.index + 1)}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # outbound internet access
