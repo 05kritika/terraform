2bcc938cf6e2a588cb657e16e0aa7a8c936e6ab8
05kritika <39408273+05kritika@users.noreply.github.com>
2019-05-10 13:12:52 +0530

    Update test.sh

--- config/test.sh
+++ config/test.sh
@@ -2,6 +2,6 @@ export AWS_REGION="us-west-2"
 
 inspec exec test/verify/ -t aws://
 
-inspec exec test/verify/test.rb -t ssh://ubuntu@"$1" -i /home/ubuntu/kritika-key.pem
+#inspec exec test/verify/test.rb -t ssh://ubuntu@"$1" -i /home/ubuntu/kritika-key.pem
 
-telnet "$2" 80
+#telnet "$2" 80
