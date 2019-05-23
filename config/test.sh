14ac7ce6cfff6cd17d164894770327ac064f2249
05kritika <39408273+05kritika@users.noreply.github.com>
2019-05-23 15:02:19 +0530

    Update test.sh

--- config/test.sh
+++ config/test.sh
@@ -1,7 +1,7 @@
 export AWS_REGION="us-west-2"
 
-inspec exec test/verify/ -t aws://
+inspec exec test/verify/ -t aws:// --reporter cli html:${pwd}/test_report1.html
 
-inspec exec test/verify/test.rb -t ssh://ubuntu@"$(cat public_dns.txt)" -i /home/ubuntu/kritika-key.pem
+inspec exec test/verify/test.rb -t ssh://ubuntu@"$(cat public_dns.txt)" -i /home/ubuntu/kritika-key.pem --reporter cli html:${pwd}/test_report2.html
 
 #telnet "$2" 80
